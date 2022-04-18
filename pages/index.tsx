1668725e0fc52815918d19534e21b5387a7d8f60
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-04-18 18:42:12 +0530

    added loading screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
+import { useSession } from 'next-auth/react'
 import DashBoard from '../components/DashBoard'
+import { useRouter } from 'next/router'
+import Loader from '../components/Loader'
 const index = () => {
+  const router = useRouter()
+  const { data: session, status } = useSession()
+  useEffect(() => {
+    if (!session) {
+      router.push('/auth/signin')
+    }
+  }, [])
+
+  if (status === 'loading') {
+    return (
+      <main className="flex h-screen items-center justify-center bg-black">
+        <Loader />
+      </main>
+    )
+  }
   return (
     <>
       <Head>
